5934c135e0831029f3504d7a69f0d587241f3f2b
dom111 <dom111@users.noreply.github.com>
2022-08-27 20:56:26 +0100

    Remove regeneration of header.html. Merges #93

--- build/examples.sh
+++ build/examples.sh
@@ -7,11 +7,3 @@ npm run --silent terser -- ./tmp/example-cdn.js -c -m -e > ./tmp/example-cdn-min
 
 printf 'javascript:' > ./examples/bookmarklet/source-min.js;
 cat ./tmp/example-cdn-min.js >> ./examples/bookmarklet/source-min.js;
-
-printf '<script type="text/javascript">' > ./examples/apache-directory-list/header.html;
-cat ./tmp/example-cdn-min.js >> ./examples/apache-directory-list/header.html;
-printf '</script><!--' >> ./examples/apache-directory-list/header.html;
-
-printf '<script type="text/javascript">' > ./examples/apache-directory-list-local/header.html;
-cat ./tmp/example-local-min.js >> ./examples/apache-directory-list-local/header.html;
-printf '</script><!--' >> ./examples/apache-directory-list-local/header.html;
